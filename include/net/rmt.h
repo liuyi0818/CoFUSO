cb8033d316137cbfad53255426e1f984a22fbc5d
liuyi-coding <liuyi_0818@hnu.edu.cn>
2020-06-09 14:49:48 +0800

    20200609v1

---
+++ include/net/rmt.h
@@ -0,0 +1,14 @@
+/*mengyuan*/
+#ifndef RMT_ENABLED
+#define RMT_ENABLED 1
+#endif
+
+#ifndef GENERATION_TYPE
+#define GENERATION_TYPE 1
+#endif
+//ly add
+#ifndef MAX_CODE_BLOCK
+#define MAX_CODE_BLOCK 30
+#endif
+//ly add
+
